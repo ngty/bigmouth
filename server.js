836b92a38a18a8bdfdbd753f98a5172c7ec16e8d
Ng Tze Yang <ngty77@gmail.com>
2013-05-22 14:47:42 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var app = require("restify").createServer();
+var config = require('./lib/config');
+var log = require('./lib/log')
+var speak = require('./lib/speak');
+var festival = require('./lib/festival');
+
+
+function handle(req, res, callback){
+  speak(req.params, function(e){
+    if(e) res.send(500, e.message);
+    else res.send(200);
+    next();
+  })
+};
+
+app.put('/:voice/speak', function(req, res, next){
+  req.params.msg = "w.t.f";
+  handle(req, res, next);
+});
+
+app.put('/speak', function(req, res, next){
+  req.params.voice = undefined
+  req.params.msg = "w.t.f";
+  handle(req, res, next);
+});
+
+app.get('/:voice/ping', function(req, res, next){
+  req.params.msg = "u ping i pong !!";
+  handle(req, res, next);
+});
+
+app.get('/ping', function(req, res, next){
+  req.params.voice = undefined
+  req.params.msg = "u ping i pong !!";
+  handle(req, res, next);
+});
+
+
+app.listen(config.public.port, function() {
+  log("bigmouth", "info", "listening at " + app.url);
+});
