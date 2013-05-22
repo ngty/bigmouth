c5ba52f367e443059c6255b4b19a345ac5ad67f7
Ng Tze Yang <ngty77@gmail.com>
2013-05-22 23:15:12 +0800

    Adapted to updated speak(), & fixed failure to populate req params.

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
-var app = require("restify").createServer();
+var restify = require('restify');
+var app = restify.createServer();
 var config = require('./lib/config');
 var log = require('./lib/log')
 var speak = require('./lib/speak');
 
 
 function handle(req, res, callback){
-  speak(req.params, function(e){
+  speak(req.params.voice, req.params.text, function(e){
     if(e) res.send(500, e.message);
     else res.send(200);
     next();
   })
 };
 
+
+app.use(restify.urlEncodedBodyParser());
+
 app.put('/:voice/speak', function(req, res, next){
-  req.params.msg = "w.t.f";
   handle(req, res, next);
 });
 
 app.put('/speak', function(req, res, next){
   req.params.voice = undefined
-  req.params.msg = "w.t.f";
   handle(req, res, next);
 });
 
 app.get('/:voice/ping', function(req, res, next){
-  req.params.msg = "u ping i pong !!";
+  req.params.text = "u ping i pong !!";
   handle(req, res, next);
 });
 
 app.get('/ping', function(req, res, next){
   req.params.voice = undefined
-  req.params.msg = "u ping i pong !!";
+  req.params.text = "u ping i pong !!";
   handle(req, res, next);
 });
 
